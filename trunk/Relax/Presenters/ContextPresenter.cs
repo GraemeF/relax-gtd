ba3eb81c0a0de65757a2f9ca090f41aea7865ffa
Graeme <Graeme@localhost>
2009-11-30 13:10:51 +0000

    Added support for renaming Contexts.

--- trunk/Relax/Presenters/ContextPresenter.cs
+++ trunk/Relax/Presenters/ContextPresenter.cs
@@ -8,11 +8,37 @@ namespace Relax.Presenters
     [PerRequest(typeof (IGtdContextPresenter))]
     public class ContextPresenter : Presenter, IGtdContextPresenter
     {
+        private bool _isReadOnly;
+
         public ContextPresenter(IGtdContext context)
         {
             Context = context;
-        }
+            _isReadOnly = true;
+       }
 
         public IGtdContext Context { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get { return _isReadOnly; }
+            set
+            {
+                if (_isReadOnly != value)
+                {
+                    _isReadOnly = value;
+                    NotifyOfPropertyChange("IsReadOnly");
+                }
+            }
+        }
+
+        public void Rename()
+        {
+            IsReadOnly = false;
+        }
+
+        public void FinishRename()
+        {
+            IsReadOnly = true;
+        }
     }
 }
