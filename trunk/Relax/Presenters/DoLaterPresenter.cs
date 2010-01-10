fd9c183008d0697dadb6954713bfd594922e6ddc
Graeme <Graeme@localhost>
2010-01-10 10:06:56 +0000

    Added DoLater presenter and view.

---
+++ trunk/Relax/Presenters/DoLaterPresenter.cs
@@ -0,0 +1,9 @@
+using Caliburn.PresentationFramework.ApplicationModel;
+using Relax.Presenters.Interfaces;
+
+namespace Relax.Presenters
+{
+    public class DoLaterPresenter : Presenter, IDoLaterPresenter
+    {
+    }
+}
