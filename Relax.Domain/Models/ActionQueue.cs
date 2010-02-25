010afe36d4caaada4bc190ecf758e7964164718a
Graeme <Graeme@localhost>
2010-02-25 18:18:44 +0000

    Added some stuff to ActionQueue.

--- Relax.Domain/Models/ActionQueue.cs
+++ Relax.Domain/Models/ActionQueue.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using Caliburn.Core.Metadata;
 using Relax.Infrastructure.Models.Interfaces;
 
@@ -7,5 +8,19 @@ namespace Relax.Domain.Models
     [PerRequest(typeof (IActionQueue))]
     public class ActionQueue : ObservableCollection<IAction>, IActionQueue
     {
+        #region IActionQueue Members
+
+        public IAction Head
+        {
+            get { return this.FirstOrDefault(); }
+        }
+
+        public void Requeue(IAction action)
+        {
+            Remove(action);
+            Add(action);
+        }
+
+        #endregion
     }
 }
--- Relax.Infrastructure/Models/Interfaces/IActionQueue.cs
+++ Relax.Infrastructure/Models/Interfaces/IActionQueue.cs
@@ -4,5 +4,7 @@ namespace Relax.Infrastructure.Models.Interfaces
 {
     public interface IActionQueue : IEnumerable<IAction>
     {
+        IAction Head { get; }
+        void Requeue(IAction action);
     }
 }
