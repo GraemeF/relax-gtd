6a07d2a3652a18ced12bd9ae6eeeee28b3cf5fc6
Graeme <Graeme@localhost>
2010-03-05 07:59:44 +0000

    Shut down the DoNow presenter when clicking Done.

--- Relax/Presenters/DoNowPresenter.cs
+++ Relax/Presenters/DoNowPresenter.cs
@@ -1,5 +1,8 @@
+using System;
 using Caliburn.Core.Metadata;
 using Caliburn.PresentationFramework.ApplicationModel;
+using Relax.Infrastructure.Models;
+using Relax.Infrastructure.Models.Interfaces;
 using Relax.Presenters.Interfaces;
 
 namespace Relax.Presenters
@@ -7,5 +10,19 @@ namespace Relax.Presenters
     [PerRequest(typeof (IDoNowPresenter))]
     public class DoNowPresenter : Presenter, IDoNowPresenter
     {
+        private readonly IAction _action;
+
+        public DoNowPresenter(IAction action)
+        {
+            _action = action;
+        }
+
+        public void Done()
+        {
+            _action.ActionState = State.Committed;
+            _action.CompletedDate = DateTime.UtcNow;
+
+            Parent.Shutdown(this);
+        }
     }
 }
--- UnitTests/Relax.Tests/Presenters/DoNowPresenterTestFixture.cs
+++ UnitTests/Relax.Tests/Presenters/DoNowPresenterTestFixture.cs
@@ -1,6 +1,36 @@
+using System;
+using Caliburn.PresentationFramework.ApplicationModel;
+using Moq;
+using Relax.Infrastructure.Models.Interfaces;
+using Relax.Presenters;
+using Relax.TestDataBuilders;
+using Xunit;
+
 namespace Relax.Tests.Presenters
 {
-    public class DoNowPresenterTestFixture
+    public class DoNowPresenterTestFixture : TestDataBuilder
     {
+        [Fact]
+        public void Done__MarksActionAsCompleted()
+        {
+            DateTime timeAtStart = DateTime.UtcNow;
+            Mock<IAction> mockAction = AnAction.Mock();
+            var test = new DoNowPresenter(mockAction.Object) {Parent = new Mock<IPresenterHost>().Object};
+
+            test.Done();
+
+            mockAction.VerifySet(x => x.CompletedDate = It.IsInRange(timeAtStart, DateTime.UtcNow, Range.Inclusive));
+        }
+
+        [Fact]
+        public void Done__ShutsDownPresenter()
+        {
+            var mockParent = new Mock<IPresenterHost>();
+            var test = new DoNowPresenter(AnAction.Build()) {Parent = mockParent.Object};
+
+            test.Done();
+
+            mockParent.Verify(x => x.Shutdown(test, It.IsAny<Action<bool>>()));
+        }
     }
 }
