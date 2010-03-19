30083770bf2540f7279072d65f2678e59e6a5e3f
Graeme <Graeme@localhost>
2010-03-19 17:57:31 +0000

    Hooked up Title

--- Relax/Presenters/ProcessActionPresenter.cs
+++ Relax/Presenters/ProcessActionPresenter.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Core.Metadata;
 using Caliburn.PresentationFramework.ApplicationModel;
+using MvvmFoundation.Wpf;
 using Relax.Infrastructure.Models.Interfaces;
 using Relax.Presenters.Interfaces;
 
@@ -9,15 +10,26 @@ namespace Relax.Presenters
     public class ProcessActionPresenter : MultiPresenter, IProcessActionPresenter
     {
         private readonly IAction _action;
+        private PropertyObserver<IAction> _propertyObserver;
 
         public ProcessActionPresenter(IAction action, IDoNowPresenter doNow, IDoLaterPresenter doLater)
         {
             _action = action;
             DoNow = doNow;
             DoLater = doLater;
+
+            _propertyObserver = new PropertyObserver<IAction>(Model).
+                RegisterHandler(x => x.Title,
+                                y => NotifyOfPropertyChange(() => DisplayName));
         }
 
         #region IProcessActionPresenter Members
+
+        public override string DisplayName
+        {
+            get { return Model.Title; }
+            set { Model.Title = value; }
+        }
 
         public IDoNowPresenter DoNow { get; private set; }
         public IDoLaterPresenter DoLater { get; private set; }
