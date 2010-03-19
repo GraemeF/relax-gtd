d1b221f9b77ea32ddf0ffaa98f6ed01a1a4c6c3e
Graeme <Graeme@localhost>
2010-03-19 22:45:20 +0000

    Changed Done to Apply.

--- Relax/Presenters/Interfaces/IDoNowPresenter.cs
+++ Relax/Presenters/Interfaces/IDoNowPresenter.cs
@@ -1,11 +1,9 @@
-using Caliburn.PresentationFramework.ApplicationModel;
-
 namespace Relax.Presenters.Interfaces
 {
     /// <summary>
     /// Presents the UI allowing the action being processed to be marked as done.
     /// </summary>
-    public interface IDoNowPresenter : IPresenter
+    public interface IDoNowPresenter : IActionProcessorPresenter
     {
     }
 }
