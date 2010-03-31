bd41027016881413b5d77c3bbb14478e62509708
Graeme <Graeme@localhost>
2010-03-31 22:55:39 +0100

    Adding missing files.

---
+++ Relax/Presenters/AlwaysSelectedPolicy.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Relax.Presenters.Interfaces;
+
+namespace Relax.Presenters
+{
+    public class AlwaysSelectedPolicy : ISelectionPolicy
+    {
+        #region ISelectionPolicy Members
+
+        public TItem ModifySelectedItem<TItem>(ISingleSelector<TItem> selector, TItem item)
+        {
+            if (item == null)
+            {
+                IModelPresenter<TItem> firstPresenter =
+                    selector.Presenters.Cast<IModelPresenter<TItem>>().FirstOrDefault();
+                if (firstPresenter != null)
+                    return firstPresenter.Model;
+            }
+            return item;
+        }
+
+        #endregion
+    }
+}
