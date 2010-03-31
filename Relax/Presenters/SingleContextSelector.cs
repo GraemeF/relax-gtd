e0a342ea9d24d1ad1cfb74e53f96e876256bdb0a
Graeme <Graeme@localhost>
2010-03-31 08:02:46 +0100

    Changed ContextsPresenter into SingleContextSelector.

--- Relax/Presenters/SingleContextSelector.cs
+++ Relax/Presenters/SingleContextSelector.cs
@@ -5,13 +5,13 @@ using Relax.Presenters.Interfaces;
 
 namespace Relax.Presenters
 {
-    [PerRequest(typeof (IContextsPresenter))]
-    public class ContextsPresenter : ListPresenter<IGtdContext, IGtdContextPresenter>, IContextsPresenter
+    [PerRequest(typeof (ISingleSelector<IGtdContext>))]
+    public class SingleContextSelector : SingleItemSelector<IGtdContext, IGtdContextPresenter>
     {
         private readonly Func<IGtdContext> _contextFactory;
         private readonly IWorkspace _workspace;
 
-        public ContextsPresenter(IWorkspace workspace,
+        public SingleContextSelector(IWorkspace workspace,
                                  Func<IGtdContext, IGtdContextPresenter> contextPresenterFactory,
                                  Func<IGtdContext> contextFactory)
             : base(workspace.Contexts, contextPresenterFactory)
